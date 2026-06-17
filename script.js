05e04741d3a98765b378bce4c93b887202adba60
gabbix11 <shitzukamai@gmail.com>
2026-06-17 12:44:58 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -13,3 +13,64 @@ function escrever() {
 }
 
 escrever();
+
+const frases = [
+    "👹 Bem-vindo ao covil do Akuma.",
+    "💀 O Entity está observando.",
+    "🎮 Sobreviva primeiro. Reclame depois.",
+    "🔥 O medo é apenas o começo.",
+    "🌙 Nem todo monstro é o killer.",
+    "🎥 Obrigado por visitar meu perfil."
+];
+const frase = document.getElementById("frase");
+
+const aleatoria = Math.floor(Math.random() * frases.length);
+
+frase.textContent = frases[aleatoria];
+
+particlesJS("particles-js", {
+    particles: {
+        number: {
+            value: 70
+        },
+
+        color: {
+            value: "#cb14dc"
+        },
+
+        shape: {
+            type: "circle"
+        },
+
+        opacity: {
+            value: 0.5
+        },
+
+        size: {
+            value: 3
+        },
+
+        move: {
+            enable: true,
+            speed: 1.5
+        }
+    },
+
+    interactivity: {
+        events: {
+            onhover: {
+                enable: true,
+                mode: "grab"
+            }
+        },
+
+        modes: {
+            grab: {
+                distance: 140,
+                line_linked: {
+                    opacity: 0.8
+                }
+            }
+        }
+    }
+});
